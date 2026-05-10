9175c077823296849ed4da555f77b8a021c70e49
Dominguez-miguel <asdalsmic@gmail.com>
2026-05-10 00:09:16 -0600

    Add files via upload

---
+++ parcial-3-mvc/examen-parcial-mvc/views/admin/deleteTorneo.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("../../controllers/torneosController.php");
+
+    $objTorneosController = new torneosController();
+    $objTorneosController->delete($_GET['id']);
+?>
+
+
+
+<!-- Dominguez Davalos Francisco Miguel -->
---
+++ parcial-3-mvc/examen-parcial-mvc/views/admin/template/footer.php
@@ -0,0 +1,43 @@
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+    <footer class="text-center text-lg-start bg-light texxt-muted">
+        <section class="p-2">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6>LOGO AQUI</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">DESARROLLO WEB AVANZADO</h6>
+                        <p>
+                            Aplicacion Web con con PHP, MySQL, PDO y BootStrap.
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">ENLACES DE INTERES</h6>
+                        <p>
+                            XXXXXXXXX.
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">CONTACTO</h6>
+                        <p>
+                            <i class="fa-solid fa-phone"></i> +52 669xxxxxxx
+                            <p></p>
+                            <i class="fa-solid fa-envelope"></i> xxxxxxx@hotmail.com
+                            <p></p>
+                            <i class="fa-solid fa-school-flag"></i> Facultad de Informatica Mazatlan, Universidad Autonoma de Sinaloa
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.25)">
+            © Clase Desarrollo Web Avanzado. MVC, PHP, PDO, MySQL, BOOTSTRAP.
+        </div>
+
+    </footer>
+</html>
+
+<!-- Dominguez Davalos Francisco Miguel -->
